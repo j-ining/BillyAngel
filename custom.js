b6f24c2dff3a4a468f15a854aaee8988e1c664f4
ina <junina93@gmail.com>
2022-10-08 17:50:41 +0900

    finish

--- custom.js
+++ custom.js
@@ -1,11 +1,21 @@
-// slide_nav
-const header = document.querySelector("header");
-const gnb = document.querySelector(".gnb");
+const nav = document.querySelector(".gnb_container");
+const gnb = document.querySelectorAll(".gnb > li");
 const lnb = document.querySelector(".lnb");
 
-header.addEventListener("mouseover", () => {
+// gnb_hover
+nav.addEventListener("mouseover", () => {
+  gnb.forEach(menu => menu.addEventListener("mouseout", () => {
+    menu.style.color = '#303030';
+  }))
+  gnb.forEach(menu => menu.addEventListener("mouseover", () => {
+    menu.style.color = '#f0616e';
+  }))
+})
+
+// lnb_slide
+nav.addEventListener("mouseover", () => {
   lnb.style.maxHeight = lnb.scrollHeight+"px";
 })
-header.addEventListener("mouseout", () => {
+nav.addEventListener("mouseout", () => {
   lnb.style.maxHeight = "0px";
 })
